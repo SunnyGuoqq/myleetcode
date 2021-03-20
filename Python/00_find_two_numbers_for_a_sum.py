7dcde596c761abaebf168d414394867c11bfdf92
Sunny Guo <sunnyguoqq@outlook.com>
2021-03-20 13:24:43 +0000

    modify some details

--- Python/00_find_two_numbers_for_a_sum.py
+++ Python/00_find_two_numbers_for_a_sum.py
@@ -1,10 +1,12 @@
 num=input('输入数字：')
 targets=input('target=')
 target=int(targets)
+
 numlist=num.split(" ")
 numlist=[int(numlist[i]) for i in range(len(numlist))]
+
 for i in range(0,len(numlist)):
-    for j in range(1,len(numlist)):
+    for j in range(i+1,len(numlist)):
         if target==numlist[i]+numlist[j]:
             print(i,j)
 
