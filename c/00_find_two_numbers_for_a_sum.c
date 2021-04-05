5d1042c826e95cfcf5961fda903f3352cfcd335c
Sunny Guo <sunnyguoqq@outlook.com>
2021-04-05 09:29:56 +0000

    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.

--- c/00_find_two_numbers_for_a_sum.c
+++ c/00_find_two_numbers_for_a_sum.c
@@ -1,22 +1,30 @@
+//author: Qingqing Guo, sunnyguoqq@outlook.com
+//iteration
+
 #include <stdio.h>
+
+/*Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. */
 
 int main()
 {
-	int nums[100];
+	int nums[100];//an array
 	int target;
-	int length;
+	int len;//length of array
 
 	printf("target=");
 	scanf("%d", &target);
 	printf("length of nums:");
-	scanf("%d", &length);
-	printf("nums[]=");
-	for(int i=0; i<=length; i++){
+	scanf("%d", &len);
+	printf("nums[len+1]=");
+
+	/*input data*/
+	for(int i=0; i<=len; i++){
 		scanf("%d", &nums[i]);
  	}	
-
-	for(int i = 0; i < length; i++){
-		for(int j = i; j < length-i; j++){
+	
+	/*find index*/
+	for(int i = 0; i < len; i++){
+		for(int j = i; j <= len; j++){
 			if(nums[i] == target-nums[j]){
 				printf("[%d,%d]",i, j);
 			}
