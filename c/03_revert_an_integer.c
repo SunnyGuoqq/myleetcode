c0c722e9cbffce31ee61d6e9ab20d85c5065e34b
Sunny Guo <sunnyguoqq@outlook.com>
2021-04-05 13:56:11 +0000

    Given an array nums and a value val, remove all instances of that value in-place and return the new length.

--- c/03_revert_an_integer.c
+++ c/03_revert_an_integer.c
@@ -1,15 +1,41 @@
+//author: Qingqing Guo, sunnyguoqq@outlook
+
 #include<stdio.h>
+
+/*Given a 32-bit signed integer, reverse digits of an integer. */
 
 int main()
 {
 	int x;
-	int a, b, c;
+	int temp = 0;
+	int res = 0;
 
+	/*input*/
 	printf("x=");
 	scanf("%d", &x);
-	a = x%10;
-	b = (x/10)%10;
-	c = x/100;
-	printf("%d", a*100+b*10+c);
+
+	/*revert*/
+	if (x < 0)
+	{
+		x = -x;
+		while (x != 0)
+		{
+			temp = x % 10;
+			temp = -temp;
+			res = res * 10 + temp;
+			x /= 10;
+		}
+		printf("%d", res);
+	}
+	else
+	{
+		while (x != 0)
+		{
+			temp = x % 10;
+			res = res * 10 + temp;
+			x /= 10;
+		}
+		printf("%d", res);
+	}
 	return 0;
 }
