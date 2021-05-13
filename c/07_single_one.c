7c553f82482565c997fe9b4acf6e45f1f1bfda62
Sunny Guo <sunnyguoqq@outlook.com>
2021-05-13 06:01:20 +0000

    modify the detail

--- c/07_single_one.c
+++ c/07_single_one.c
@@ -1,33 +1,35 @@
-//author: Qingqing Guo, sunnyguoqq@outlook
-
 #include<stdio.h>
 #define MAXSIZE 100
 
-/*Given a non-empty array of integers nums, 
- * every element appears twice except for one. 
+/*Given a non-empty array of integers nums,
+ * every element appears twice except for one.
  * Find that single one.  */
 
 int main()
 {
 	int nums[MAXSIZE];
-	int len, x;
+	int number, x;
 
 	/*input data*/
-	printf("The length of nums:");
-	scanf_s("%d", &len);
-	printf("nums[len+1]=(digital)");
-	for (int i = 0; i <= len; i++)
+	printf("The program is used to check if there is a single digit!\n");
+	printf("Please enter the number of digits: ");
+	scanf("%d", &number);
+	printf("Please enter the digits(number+1): ");
+	for (int i = 0; i <= number; i++)
 	{
-		scanf_s("%d", &nums[i]);
+		scanf("%d", &nums[i]);
 	}
 
 	/*XOR*/
 	x = nums[0];
-	for (int j = 1; j <= len; j++)
+	for (int j = 1; j <= number; j++)
 	{
 		x = x ^ nums[j];
 	}
-	printf("%d",x);
+	if (x == 0)
+		printf("There is no single digit in the number entered!");
+	else 
+		printf("A single number is %d", x);
 	return 0;
 }
 
